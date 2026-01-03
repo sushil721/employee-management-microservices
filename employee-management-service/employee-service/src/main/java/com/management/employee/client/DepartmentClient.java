01902214a5721058253aaf95639cb04dfb300bd2
sushil721 <singh.sushil721@gmail.com>
2026-01-04 01:44:44 +0530

    Loadbalancing with Eureka, OpenFeign, and Spring Cloud Load balancer

--- employee-management-service/employee-service/src/main/java/com/management/employee/client/DepartmentClient.java
+++ employee-management-service/employee-service/src/main/java/com/management/employee/client/DepartmentClient.java
@@ -7,7 +7,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(url = "http://localhost:8081", value = "DEPARTMENT-SERVICE")
+//@FeignClient(url = "http://localhost:8081", value = "DEPARTMENT-SERVICE")
+// using name of service we can achieve load balancer.
+// here, Spring Cloud Netflix Eureka dependency have spring cloud load balancer.
+// which will balance all load over the different instances of application.
+@FeignClient(name = "DEPARTMENT-SERVICE")
 public interface DepartmentClient {
 
     @GetMapping("api/departments/{departmentCode}")
