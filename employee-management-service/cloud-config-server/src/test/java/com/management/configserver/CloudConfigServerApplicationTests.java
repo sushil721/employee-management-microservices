c040bd997298d38dfcaa018e06f8bccf7c5a2a35
sushil721 <singh.sushil721@gmail.com>
2026-01-13 13:14:27 +0530

    Spring Cloud Config Server service

---
+++ employee-management-service/cloud-config-server/src/test/java/com/management/configserver/CloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.management.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
